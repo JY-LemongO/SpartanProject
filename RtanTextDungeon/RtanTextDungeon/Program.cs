f888cfb473b139c9212466dc307e148c9c0dcfc9
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2023-12-29 15:15:33 +0900

    상태창 적용

--- RtanTextDungeon/RtanTextDungeon/Program.cs
+++ RtanTextDungeon/RtanTextDungeon/Program.cs
@@ -23,6 +23,17 @@ namespace RtanTextDungeon
             public int gold;
         }
 
+        static void CharacterCreate(out Player player)
+        {
+            player.lv = 1;
+            player.name = "십칠조";
+            player.m_class = PlayerClass.Worrior;
+            player.atk = 10;
+            player.def = 5;
+            player.hp = 100;
+            player.gold = 1500;
+        }
+
         static void SelectAction(ref Player player)
         {
             while (true)
@@ -61,6 +72,87 @@ namespace RtanTextDungeon
             }                   
         }
 
+        static void Status(ref Player player)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[상태 창]\n");
+                Console.ResetColor();
+
+                Console.WriteLine("---------------------------------\n");
+                Console.WriteLine($"Lv. {player.lv.ToString("00")}\n\n" +
+                                $"{player.name} ({player.m_class})\n" +
+                                $"공격력 : {player.atk}\n" +
+                                $"방어력 : {player.def}\n" +
+                                $"체 력 : {player.hp}\n" +
+                                $"Gold : {player.gold}\n");
+                Console.WriteLine("---------------------------------\n");
+
+                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
+                Console.WriteLine("(1) : [인벤토리]\n(2) : [상점]\n(3) : [마을로 돌아가기]\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Inventory(ref player);
+                        return;
+                    case "2":
+                        Console.Clear();
+                        Shop(ref player);
+                        return;
+                    case "3":
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!!!잘못된 입력입니다!!!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+
+        static void Inventory(ref Player player)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[인벤토리]\n");
+                Console.ResetColor();
+
+                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
+                Console.WriteLine("(1) : [상태]\n(2) : [상점]\n(3) : [마을로 돌아가기]\n");
+
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Status(ref player);
+                        return;
+                    case "2":
+                        Console.Clear();
+                        Shop(ref player);
+                        return;
+                    case "3":
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!!!잘못된 입력입니다!!!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+
         static void Shop(ref Player player)
         {
             while (true)
@@ -94,89 +186,6 @@ namespace RtanTextDungeon
                         break;
                 }
             }
-        }
-
-        static void Inventory(ref Player player)
-        {
-            while (true)
-            {                
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("[인벤토리]\n");
-                Console.ResetColor();
-
-                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
-                Console.WriteLine("(1) : [상태]\n(2) : [상점]\n(3) : [마을로 돌아가기]\n");
-
-                string? input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        Console.Clear();
-                        Status(ref player);
-                        return;
-                    case "2":
-                        Console.Clear();
-                        Shop(ref player);
-                        return;
-                    case "3":
-                        Console.Clear();
-                        return;
-                    default:
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("!!!잘못된 입력입니다!!!");
-                        Console.ResetColor();
-                        break;
-                }
-            }
-        }
-
-        static void Status(ref Player player)
-        {
-            while (true)
-            {                
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("[상태 창]\n");
-                Console.ResetColor();                
-
-                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
-                Console.WriteLine("(1) : [인벤토리]\n(2) : [상점]\n(3) : [마을로 돌아가기]\n");
-
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
-
-                string? input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        Console.Clear();
-                        Inventory(ref player);
-                        return;
-                    case "2":
-                        Console.Clear();
-                        Shop(ref player);
-                        return;
-                    case "3":
-                        Console.Clear();
-                        return;
-                    default:
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("!!!잘못된 입력입니다!!!");
-                        Console.ResetColor();
-                        break;
-                }
-            }
-        }
-
-        static void CharacterCreate(out Player player)
-        {
-            player.lv = 1;
-            player.name = "십칠조";
-            player.m_class = PlayerClass.Worrior;
-            player.atk = 10;
-            player.def = 5;
-            player.hp = 100;
-            player.gold = 1500;
         }
 
         static void Main(string[] args)
--- RtanTextDungeon/RtanTextDungeon/obj/Debug/net8.0/RtanTextDungeon.AssemblyInfo.cs
+++ RtanTextDungeon/RtanTextDungeon/obj/Debug/net8.0/RtanTextDungeon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RtanTextDungeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22dd4d82732279df1a26aa50f76c37b60f40c133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49044bf995a3c578af3cf68aef9c26bc21abe5ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("RtanTextDungeon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RtanTextDungeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
