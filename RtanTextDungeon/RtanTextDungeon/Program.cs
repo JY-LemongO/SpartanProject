49044bf995a3c578af3cf68aef9c26bc21abe5ad
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2023-12-29 14:49:58 +0900

    입력에 따라 창 변경
    캐릭터 생성(기본값) 추가
    입력에 따라
    상태, 인벤토리, 상점 창 전환

---
+++ RtanTextDungeon/RtanTextDungeon/Program.cs
@@ -0,0 +1,190 @@
+﻿using System.Numerics;
+
+namespace RtanTextDungeon
+{
+    public enum PlayerClass
+    {
+        Worrior,
+        Archer,
+        Magic,
+        Thief,
+    }
+
+    internal class Program
+    {
+        struct Player
+        {
+            public int lv;
+            public string name;
+            public PlayerClass m_class;
+            public int atk;
+            public int def;
+            public int hp;
+            public int gold;
+        }
+
+        static void SelectAction(ref Player player)
+        {
+            while (true)
+            {                
+                Console.WriteLine("※※※17초 던전 마을에 온 것을 환영합니다.※※※");
+                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
+                Console.WriteLine("(1) : [상태]\n(2) : [인벤토리]\n(3) : [상점]\n");                
+
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Status(ref player);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Inventory(ref player);
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Shop(ref player);
+                        break;
+                    case "4":                        
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine("※※※게임을 종료합니다※※※");
+                        Console.ResetColor();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!!!잘못된 입력입니다!!!");
+                        Console.ResetColor();
+                        break;
+                }
+            }                   
+        }
+
+        static void Shop(ref Player player)
+        {
+            while (true)
+            {                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[상점]\n");
+                Console.ResetColor();
+
+                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
+                Console.WriteLine("(1) : [상태]\n(2) : [인벤토리]\n(3) : [마을로 돌아가기]\n");
+
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Status(ref player);
+                        return;
+                    case "2":
+                        Console.Clear();
+                        Shop(ref player);
+                        return;
+                    case "3":
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!!!잘못된 입력입니다!!!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+
+        static void Inventory(ref Player player)
+        {
+            while (true)
+            {                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[인벤토리]\n");
+                Console.ResetColor();
+
+                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
+                Console.WriteLine("(1) : [상태]\n(2) : [상점]\n(3) : [마을로 돌아가기]\n");
+
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Status(ref player);
+                        return;
+                    case "2":
+                        Console.Clear();
+                        Shop(ref player);
+                        return;
+                    case "3":
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!!!잘못된 입력입니다!!!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+
+        static void Status(ref Player player)
+        {
+            while (true)
+            {                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[상태 창]\n");
+                Console.ResetColor();                
+
+                Console.WriteLine("※※※원하시는 행동을 선택하세요.※※※\n");
+                Console.WriteLine("(1) : [인벤토리]\n(2) : [상점]\n(3) : [마을로 돌아가기]\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Inventory(ref player);
+                        return;
+                    case "2":
+                        Console.Clear();
+                        Shop(ref player);
+                        return;
+                    case "3":
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!!!잘못된 입력입니다!!!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+
+        static void CharacterCreate(out Player player)
+        {
+            player.lv = 1;
+            player.name = "십칠조";
+            player.m_class = PlayerClass.Worrior;
+            player.atk = 10;
+            player.def = 5;
+            player.hp = 100;
+            player.gold = 1500;
+        }
+
+        static void Main(string[] args)
+        {
+            Player player;
+            CharacterCreate(out player);
+
+            SelectAction(ref player);
+        }
+    }
+}
